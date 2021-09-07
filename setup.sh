4f7e1d27d347e9705b4fcac4e4c9516697d397f1
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-09-07 20:16:35 +0600

    dumpyara: Install extract-dtb from PyPI

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    if [[ "$(command -v apt)" != "" ]]; then
+        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename
+    elif [[ "$(command -v pacman)" != "" ]]; then
+        sudo pacman -Sy --noconfirm unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio
+    fi
+    PIP=pip3
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install protobuf xz brotli lz4 aria2
+    PIP=pip
+fi
+
+sudo "$PIP" install backports.lzma extract-dtb protobuf pycrypto docopt zstandard twrpdtgen
